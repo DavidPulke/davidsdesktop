99a31322b63c1eb2375274761b4f0ef54a000c1e
David Polak <davidpolak10201@gmail.com>
2025-01-19 06:07:26 +0200

    debaging the build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     define: {
-        'process.env': process.env,
+        'process.env': {
+            VITE_WEATHER_KEY: JSON.stringify(process.env.VITE_WEATHER_KEY),
+            VITE_API_CLIENT_ID: JSON.stringify(process.env.VITE_API_CLIENT_ID),
+            VITE_API_CLIENT_KEY: JSON.stringify(process.env.VITE_API_CLIENT_KEY),
+            VITE_API_YOUTUBE_KEY: JSON.stringify(process.env.VITE_API_YOUTUBE_KEY),
+        },
     },
 });
+
 
